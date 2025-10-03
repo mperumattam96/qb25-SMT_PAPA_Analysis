2a9994bba10da0893c4b128f3ddccd8839a3faff
mperumattam96 <mperumattam@gmail.com>
2025-10-03 11:38:16 -0400

    updated script with NumPy and argeparse packages

--- code/script.py
+++ code/script.py
@@ -1,1 +1,11 @@
-# 
+# Using a Python script, read in the CSV file obtained from SMT data 
+# and tabulate the number of localizations per frame 
+# focus on columns x, y, frame, and trajectory
+# NumPy package
+# argparse package
+
+#!/usr/bin/env python3
+
+import numpy as np 
+import argparse
+
